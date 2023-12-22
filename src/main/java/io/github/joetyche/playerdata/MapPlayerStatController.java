4105b56e669f032db905948d239c71159e92f5ce
joetyche <42421751+joetyche@users.noreply.github.com>
2023-12-22 15:50:39 +0000

    Create interfaces and rough implementations of a map cache system and sql push/pull system

---
+++ src/main/java/io/github/joetyche/playerdata/MapPlayerStatController.java
@@ -0,0 +1,63 @@
+package io.github.joetyche.playerdata;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import org.bukkit.Bukkit;
+
+/**
+ * This class provides a HashMap of UUIDs against statMaps (a map of stat IDs to values). It's
+ * purpose is to reduce SQL queries by caching results.
+ */
+public abstract class MapPlayerStatController implements IPlayerStatController {
+
+    private final HashMap<UUID, Map<Long, Double>> playerStatMap;
+
+
+    public MapPlayerStatController() {
+        playerStatMap = new HashMap<>();
+    }
+
+
+    @Override
+    public void setStat(UUID uuid, long statID, double value) {
+        // If player is not in playerStatMap, create empty HashMap for them now and store it
+        if (playerStatMap.containsKey(uuid)) {
+            playerStatMap.put(uuid, new HashMap<>());
+        }
+
+        // Get player's map
+        Map<Long, Double> statMap = playerStatMap.get(uuid);
+        if (statMap == null) {
+            // Warn if still not found
+            Bukkit.getLogger()
+                    .warning("MapPlayerStatController tried to set " + statID + " to " + value
+                            + " for " + uuid
+                            + ", but could not find the player's statMap in the playerStatMap");
+            return;
+        }
+
+        // Update player's stats
+        statMap.put(statID, value);
+    }
+
+    @Override
+    public double getStat(UUID uuid, long statID, double defaultValue) {
+        Map<Long, Double> statMap = playerStatMap.get(uuid);
+        if (statMap != null) {
+            if (statMap.containsKey(statID)) {
+                // If key is present, return the value
+                return statMap.get(statID);
+            }
+        }
+        // If the player is not stored in the map or the key is not present
+        // return the default value
+        return defaultValue;
+    }
+
+    protected boolean playerHasStatLoaded(UUID uuid, long statID) {
+        return playerStatMap.containsKey(uuid) ? playerStatMap.get(uuid).containsKey(statID)
+                : false;
+    }
+
+}
---
+++ src/main/java/io/github/joetyche/playerdata/SqlPlayerStatController.java
@@ -0,0 +1,61 @@
+package io.github.joetyche.playerdata;
+
+import java.util.UUID;
+import io.github.joetyche.util.StatCounterConfig;
+
+/**
+ * This class handles the uploading and downloading of player's data to and from the SQL database,
+ * whilst also updating the map cache.
+ */
+public class SqlPlayerStatController extends MapPlayerStatController {
+
+    private StatCounterConfig statCounterConfig;
+
+
+    public SqlPlayerStatController(StatCounterConfig statCounterConfig) {
+        this.statCounterConfig = statCounterConfig;
+    }
+
+    @Override
+    public void setStat(UUID uuid, long statID, double value) {
+        // TODO: In future, can do configurable option whether to upload to SQL
+        // every time stats are set, periodically, etc.
+        // For now, assume every time.
+        // Upload stat data to SQL
+        setStatSQL(uuid, statID, value);
+
+        // Set in local HashMap too:
+        super.setStat(uuid, statID, value);
+    }
+
+    @Override
+    public double getStat(UUID uuid, long statID, double defaultValue) {
+        // If stat not present in local HashMap
+        if (!super.playerHasStatLoaded(uuid, statID)) {
+            // Download stat data from SQL
+            double storedSQLValue = getStatSQL(uuid, statID, defaultValue);
+
+            // Set in local HashMap too:
+            super.setStat(uuid, statID, storedSQLValue);
+        }
+
+        // Return stat data
+        return super.getStat(uuid, statID, defaultValue);
+    }
+
+    @Override
+    public double getStat(UUID uuid, long statID) {
+        return getStat(uuid, statID, statCounterConfig.getDefaultStatValue());
+    }
+
+    private void setStatSQL(UUID uuid, long statID, double value) {
+        // TODO: Set stat data in SQL
+    }
+
+
+    public double getStatSQL(UUID uuid, long statID, double defaultvalue) {
+        // TODO: Retrieve stat data from SQL
+        return 0;
+    }
+
+}
