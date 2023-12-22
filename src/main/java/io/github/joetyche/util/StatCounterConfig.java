01f72cf80113fd8e3a430956be51c239759bfc81
joetyche <42421751+joetyche@users.noreply.github.com>
2023-12-22 15:50:23 +0000

    Create Config handler class

---
+++ src/main/java/io/github/joetyche/util/StatCounterConfig.java
@@ -0,0 +1,22 @@
+package io.github.joetyche.util;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import io.github.joetyche.StatCounter;
+
+public class StatCounterConfig {
+
+    private FileConfiguration getConfig() {
+        return plugin.getConfig();
+    }
+
+    private StatCounter plugin;
+
+    public StatCounterConfig(StatCounter plugin) {
+        this.plugin = plugin;
+    }
+
+    public int getDefaultStatValue() {
+        return plugin.getConfig().getInt("Default stat value", 0);
+    }
+
+}
